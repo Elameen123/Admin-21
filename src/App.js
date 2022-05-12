37c54dc4e0c763c10c55aa206b1d993ca997ea5d
Ukwuoma Tochukwu <ukwuomatochi112@gmail.com>
2022-05-12 19:39:26 +0100

    Dev Enviro Set

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Login from './Pages/Login/Login';
+import Dashboard from './Pages/Dashboard'
+import ProtectedRoutes from './Component/ProtectedRoutes';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path='/Login' element={<Login />}/>
+          <Route element={<ProtectedRoutes /> } >
+              <Route path="/*" element={<Dashboard  />}/> 
+            </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
