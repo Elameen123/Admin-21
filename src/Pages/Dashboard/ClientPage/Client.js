94a86ddfd6a5dbf254fc139104ecf69896d1aab4
Elameen123 <69402639+Elameen123@users.noreply.github.com>
2022-05-19 17:09:51 +0100

    Client Page

--- src/Pages/Dashboard/ClientPage/Client.js
+++ src/Pages/Dashboard/ClientPage/Client.js
@@ -1,9 +1,69 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import Dashboard from "../../../Component/SideNav/Dashboard";
+import './Client.css';
 
-const Client = () => {
-  return (
-    <div>Client</div>
+const url = ' https://fakerapi.it/api/v1/companies?_quantity=8'
+
+function Client() {
+  const [loading,setLoading] = useState(true)
+  const [CusInfo,setCusInfo] = useState({})
+  const [value,setValue] = useState(0)
+
+  const fetchInfo = async ()=>{
+    const response = await fetch(url)
+    const info = await response.json()
+    setCusInfo(info.data)
+    setLoading(false)
+    console.log(info.data)
+  }
+
+useEffect(()=>{
+  fetchInfo()
+},[value])
+
+if(loading){
+  return(
+    <section className="">
+      <h1>....Loading.....</h1>
+    </section>
   )
 }
+// const {name,email,phone,contact,image} = CusInfo[value]
 
-export default Client
+
+  return (
+    <section >
+      <div className="contact-div">
+        <Dashboard></Dashboard>
+      
+      <table>
+        <thead>
+          <tr>
+          <th>Full name</th>
+          <th>email</th>
+          <th>phone number</th>
+          <th>gender</th>
+          <th>image</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            CusInfo ? CusInfo.map((CusInfo, idx) => (
+              <tr>
+                <td>{CusInfo.name}</td>
+                <td>{CusInfo.email}</td>
+                <td>{CusInfo.phone}</td>
+                <td>{CusInfo.contact.gender}</td>
+                <td><img src={CusInfo.image} alt="image" width={60}/></td>
+              </tr> 
+            )) : null
+          }
+      
+        </tbody>
+      </table>
+      </div> 
+    </section>
+  );
+}
+
+export default Client;
